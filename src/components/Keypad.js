5265ccfde2a8358f4a338853c3c7ccf3774fee1c
Conor Murnane <conormurnane11@gmail.com>
2021-02-02 07:25:09 -0500

    commit tree 7503f6ea2cc7c1681607df65b0742e7d45c803bc

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,12 @@
 // Code Keypad Component Here
+function Keypad(){
+    function typeInput(event){
+        console.log("Entering password...")
+    }
+
+    return(
+        <input type = "password" placeholder = "type something" onChange = {typeInput}/>
+    )
+}
+
+export default Keypad;
